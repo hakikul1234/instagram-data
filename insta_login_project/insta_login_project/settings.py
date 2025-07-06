4cb7bbdcc55db1400aefaeaf133786f1b0ee115d
hakikul1234 <thehakikul@gmail.com>
2025-07-06 15:40:34 +0530

    Debug change

--- insta_login_project/insta_login_project/settings.py
+++ insta_login_project/insta_login_project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-j38*ibiodo*5-b%_dd23vtieb2)7o#9l3_#j5%ys)9vehbb6*n'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
@@ -74,11 +74,13 @@ WSGI_APPLICATION = 'insta_login_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+import dj_database_url
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        conn_max_age=600,
+        ssl_require=True
+    )
 }
 
 
